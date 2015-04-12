9c795474e2747f60eca38259eee65aca2a1d62d6
tjin.1995@gmail.com <tjin.1995@gmail.com>
2015-04-12 18:41:42 -0400

    load map to current location

--- src/com/example/soball/MapActivity.java
+++ src/com/example/soball/MapActivity.java
@@ -1,8 +1,11 @@
 package com.example.soball;
 
 //import com.example.mapdemo.R;
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -16,35 +19,87 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import android.support.v7.app.ActionBarActivity;
 import android.app.Activity;
 import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MapActivity extends Activity implements OnMapReadyCallback{
+public class MapActivity extends Activity implements
+	OnMapReadyCallback, ConnectionCallbacks, OnConnectionFailedListener, LocationListener{
 
-	GoogleApiClient mGoogleApiClient;
+	private GoogleApiClient mGoogleApiClient;
+	private Location mLastLocation;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_map);
 		
-		
+		buildGoogleApiClient();
+		mGoogleApiClient.connect();
 		MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
 		mapFragment.getMapAsync( this);
 
+		 
+	}
+	
+//	@Override
+//	protected void onResume(){
+//		mGoogleApiClient.connect();
+//	}
+	@Override
+	public void onMapReady(GoogleMap map) {
+    	map.setMyLocationEnabled(true);
+
+		if (mLastLocation != null){
+			double lat = mLastLocation.getLatitude();
+			double lng = mLastLocation.getLongitude();
+			
+	    	map.moveCamera( CameraUpdateFactory.newLatLngZoom(new LatLng(lat, lng) , 14.0f));
+		}else{
+	    	map.moveCamera( CameraUpdateFactory.newLatLngZoom(new LatLng(0, 0) , 14.0f) );
+		}
+
+    }
+	
+	protected synchronized void buildGoogleApiClient() {
+	    mGoogleApiClient = new GoogleApiClient.Builder(this)
+	        .addConnectionCallbacks(this)
+	        .addOnConnectionFailedListener(this)
+	        .addApi(LocationServices.API)
+	        .build();
+	}
+	
+	@Override
+    public void onConnected(Bundle connectionHint) {
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        if (mLastLocation != null) {
+        	double lat = mLastLocation.getLatitude();
+    		double lng = mLastLocation.getLongitude();
+    		MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+    		mapFragment.getMap().moveCamera( CameraUpdateFactory.newLatLngZoom(new LatLng(lat, lng) , 14.0f));
+        }
+    }
+	@Override
+	public void onConnectionFailed(ConnectionResult arg0) {
+		// TODO Auto-generated method stub
 		
 	}
 	@Override
-	public void onMapReady(GoogleMap map) {
+	public void onConnectionSuspended(int arg0) {
+		// TODO Auto-generated method stub
 		
-
-    	//Location currLocation = map.getMyLocation();
-    	map.setMyLocationEnabled(true);
-//    	double lat = currLocation.getLatitude();
-//    	double lng = currLocation.getLongitude();
-    	map.moveCamera( CameraUpdateFactory.newLatLngZoom(new LatLng(43.789366, -79.360293) , 14.0f) );
-    }
+	}
+	
+	@Override
+	public void onLocationChanged(Location location) {
+		double lat = mLastLocation.getLatitude();
+		double lng = mLastLocation.getLongitude();
+		MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+		mapFragment.getMap().moveCamera( CameraUpdateFactory.newLatLngZoom(new LatLng(lat, lng) , 14.0f));
+	    //handleNewLocation(location);
+	}
 	
 	
 //	@Override
